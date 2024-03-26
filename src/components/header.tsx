ecde3c87e712871a2ee7590a3e110a061256e6d7
bp713 <pepersben@gmail.com>
2024-03-26 11:47:20 +0000

    change refresh button to be mui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectNumberOfMovies } from "../features/movieReducer";
 import { loadMovies } from "../features/movieReducer";
 import { useAppDispatch } from "../app/hooks";
+import Button from "@mui/material/Button";
 
 const useLoadMovies = () => {
     const dispatch = useAppDispatch();
@@ -16,7 +17,9 @@ export const Header = () => {
     return (
         <div>
             <h2>Welcome to Movie database!</h2>
-            <button onClick={loadMovies}>Refresh</button>
+            <Button color="primary" variant="contained" onClick={loadMovies}>
+                Refresh
+            </Button>
             <p>Total movies displayed {numberOfMovies}</p>
         </div>
     );
